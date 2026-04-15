0decf6d3a09095e90f0a57086a976595a2aef491
andresloja16 <lojaandres529@gmail.com>
2026-04-15 11:23:19 -0500

    corregido

--- check_columns.py
+++ check_columns.py
@@ -1,10 +1,10 @@
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname='base_prueba', user='postgres', password='14789632', host='localhost')
+    conn = psycopg2.connect(dbname='datawarehouse_ja', user='publica_contenido', password='publica_contenido', host='172.18.46.16', port='6432')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM series LIMIT 0;")
+    cursor.execute("SELECT * FROM jdw_dashboards.transaccion_perfil_riesgo LIMIT 0;")
     colnames = [desc[0] for desc in cursor.description]
-    print("Columnas en Postgres para la tabla 'series':", colnames)
+    print("Columnas en Postgres para la tabla 'jdw_dashboards.transaccion_perfil_riesgo':", colnames)
     cursor.close()
     conn.close()
 except Exception as e:
